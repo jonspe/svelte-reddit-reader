fead2b0c840a79bca80604fb33b37c1b59c4ff39
Joona Perasto <joona.perasto@tuni.fi>
2021-01-02 07:27:18 +0200

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Header from "./Header.svelte"
+	import Listing from "./Listing.svelte"
+</script>
+
+<Header></Header>
+<main>
+	<h1>Simple reddit reader.</h1>
+	<Listing></Listing>
+</main>
+<footer>
+	Made with Svelte, by Joona Perasto. 2021.
+</footer>
+
+<style>
+	:global(body) {
+		padding: 0;
+		background: #f4f4f4;
+	}
+	main {
+		padding: 0 6rem;
+		margin: 6rem 0;
+	}
+	footer {
+		text-align: center;
+		padding: 1.2rem 0;
+	}
+	@media (max-width: 900px) {
+		main {
+			padding: 0 1rem;
+		}
+	}
+</style>
---
+++ src/Header.svelte
@@ -0,0 +1,47 @@
+<script>
+</script>
+
+<header>
+	<nav class="parent">
+		<nav class="browsing">
+			<a href="yes">Home</a>
+			<a href="yes">Popular</a>
+			<a href="yes">All</a>
+			<a href="yes">Random</a>
+		</nav>
+		<nav class="options">
+			<a href="yes">Login</a>
+			<a href="yes">Preferences</a>
+		</nav>
+	</nav>
+</header>
+
+<style>
+	header {
+		position: fixed;
+		left: 0;
+		right: 0;
+		top: 0;
+		background: white;
+		padding: 1rem 6rem;
+		box-shadow: 0 -4px 8px 4px rgba(0, 0, 0, 0.1);
+		z-index: 3;
+	}
+	nav.parent {
+		display: flex;
+		justify-content: space-between;
+	}
+	nav.parent > nav {
+		display: flex;
+		gap: 1rem;
+	}
+	nav a {
+		color: black;
+		font-size: 1rem;
+	}
+	@media (max-width: 900px) {
+		header {
+			padding: 1rem 1rem;
+		}
+	}
+</style>
