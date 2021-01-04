5661943cff4fc4513c58fdd2a28de5a3b2eecba6
Joona Perasto <joona.perasto@tuni.fi>
2021-01-04 02:57:14 +0200

    Add scroll to bottom check

--- src/components/Listing.svelte
+++ src/components/Listing.svelte
@@ -7,8 +7,22 @@
       .then((data) => data.data.children)
   }
 
+  let lastRefreshed = 0
+  const handleScroll = function (event) {
+    const currentTime = Date.now()
+    if (
+      currentTime - lastRefreshed > 2000 &&
+      window.innerHeight + window.pageYOffset >= document.body.offsetHeight
+    ) {
+      console.log('Bottom reached! Load more!')
+      lastRefreshed = currentTime
+    }
+  }
+
   let promise = getPosts('https://www.reddit.com/')
 </script>
+
+<svelte:window on:scroll={handleScroll} />
 
 {#await promise then posts}
   {#each posts as post}
