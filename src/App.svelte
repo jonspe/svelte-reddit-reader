978ede4226e49c758c2740b7a7fba874f6273228
Joona Perasto <joona.perasto@tuni.fi>
2021-08-13 00:57:39 +0300

    Add navbar for frontpage and themes

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,12 @@
   import Post from './components/Post.svelte';
   import Listing from './components/Listing.svelte';
   import { listingFetchPath, postPath } from './stores';
+  import Navbar from './components/Navbar.svelte';
 </script>
 
 <header>
   <h1>reddit reader</h1>
+  <Navbar />
 </header>
 <main>
   {#if $postPath}
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<script>
+  import themes from '../themes.json';
+</script>
+
+<nav>
+  <a href="#/">frontpage</a>
+  {#each Object.entries(themes) as [theme, list]}
+    <a href={'#/theme/' + theme}>{theme}</a>
+  {/each}
+</nav>
+
+<style>
+  nav a {
+    display: inline-block;
+    margin-right: 1rem;
+    margin-bottom: 0.4rem;
+  }
+</style>
